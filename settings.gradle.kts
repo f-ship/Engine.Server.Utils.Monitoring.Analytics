fa7d5439e52837abebfef9cd028a48843bcaed60
Sam Taiwo <staiwo@onlydates.fan>
2024-12-24 21:38:42 +0000

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "analytics"
+rootProject.buildFileName = "engine.server.utils.monitoring.analytics.gradle.kts"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
